a99457db0b74c04f46fd86abbb3ee5440a8793c9
fgeerts <fgeerts@bol.com>
2015-09-30 16:07:27 +0200

    added content wrapper to pages

--- dist/templates/header-template.js
+++ dist/templates/header-template.js
@@ -1,6 +1,6 @@
 (function() {
   var template = Handlebars.template, templates = Handlebars.templates = Handlebars.templates || {};
 templates['header-template'] = template({"compiler":[7,">= 4.0.0"],"main":function(container,depth0,helpers,partials,data) {
-    return "<div class=\"page_header\">\r\n    <h1><a href=\"/index.html\" class=\"noDecoration\">Voika's Sandbox</a></h1>\r\n</div>\r\n";
+    return "<div class=\"pageHeader_wrapper\">\r\n    <div class=\"menuBar\">\r\n    	<h1><a href=\"/index.html\" class=\"noDecoration\">Voika's Sandbox</a></h1>\r\n    </div>\r\n</div>\r\n";
 },"useData":true});
 })();
