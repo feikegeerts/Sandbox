a8aa9d5a390965842cc92bda13ed8f1e73f7f6dd
fgeerts <fgeerts@bol.com>
2015-09-30 11:52:55 +0200

    Added distribution folder to repository

---
+++ dist/templates/animals-template.js
@@ -0,0 +1,19 @@
+(function() {
+  var template = Handlebars.template, templates = Handlebars.templates = Handlebars.templates || {};
+templates['animals-template'] = template({"1":function(container,depth0,helpers,partials,data) {
+    var helper, alias1=depth0 != null ? depth0 : {}, alias2=helpers.helperMissing, alias3="function", alias4=container.escapeExpression;
+
+  return "		<li class=\"animal\">"
+    + alias4(((helper = (helper = helpers.type || (depth0 != null ? depth0.type : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"type","hash":{},"data":data}) : helper)))
+    + " says "
+    + alias4(((helper = (helper = helpers.sound || (depth0 != null ? depth0.sound : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"sound","hash":{},"data":data}) : helper)))
+    + "</li>\r\n";
+},"compiler":[7,">= 4.0.0"],"main":function(container,depth0,helpers,partials,data) {
+    var stack1, helper, options, buffer = 
+  "<ul>\r\n";
+  stack1 = ((helper = (helper = helpers.animals || (depth0 != null ? depth0.animals : depth0)) != null ? helper : helpers.helperMissing),(options={"name":"animals","hash":{},"fn":container.program(1, data, 0),"inverse":container.noop,"data":data}),(typeof helper === "function" ? helper.call(depth0 != null ? depth0 : {},options) : helper));
+  if (!helpers.animals) { stack1 = helpers.blockHelperMissing.call(depth0,stack1,options)}
+  if (stack1 != null) { buffer += stack1; }
+  return buffer + "</ul>";
+},"useData":true});
+})();
---
+++ dist/templates/header-template.js
@@ -0,0 +1,6 @@
+(function() {
+  var template = Handlebars.template, templates = Handlebars.templates = Handlebars.templates || {};
+templates['header-template'] = template({"compiler":[7,">= 4.0.0"],"main":function(container,depth0,helpers,partials,data) {
+    return "<div class=\"page_header\">\r\n    <h1><a href=\"/index.html\" class=\"noDecoration\">Voika's Sandbox</a></h1>\r\n</div>\r\n";
+},"useData":true});
+})();
