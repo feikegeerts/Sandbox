c228e0bf638162a8b4e5997b2d221bec7b790399
fgeerts <fgeerts@bol.com>
2015-10-12 12:57:32 +0200

    Updated footer content and styling

--- dist/templates/handlebars-templates.js
+++ dist/templates/handlebars-templates.js
@@ -17,7 +17,7 @@ templates['animals-template'] = template({"1":function(container,depth0,helpers,
   return buffer + "</ul>";
 },"useData":true});
 templates['footer-template'] = template({"compiler":[7,">= 4.0.0"],"main":function(container,depth0,helpers,partials,data) {
-    return "<div class=\"footer_wrapper\">\r\n	<div class=\"content\">\r\n		<p><a href=\"/\">Home</a><p>\r\n		<p>Website by: Feike Geerts</p>\r\n	</div>\r\n</div>";
+    return "<div class=\"footer_wrapper\">\r\n	<div class=\"content\">\r\n		<p><a href=\"/\">Home</a></p>\r\n		<p>Copyright © 2015-2016 Feike's Sandbox. All Rights Reserved.</p>\r\n	</div>\r\n</div>";
 },"useData":true});
 templates['header-template'] = template({"compiler":[7,">= 4.0.0"],"main":function(container,depth0,helpers,partials,data) {
     return "<div class=\"pageHeader_wrapper\">\r\n    <div class=\"menuBar\">\r\n    	<h1><a href=\"/index.html\" class=\"noDecoration\">Feike's Sandbox</a></h1>\r\n    </div>\r\n</div>\r\n\r\n";
