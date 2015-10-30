c941dcf2b7a50d07fb81f8743477fb2b60b5495b
fgeerts <fgeerts@bol.com>
2015-10-30 09:53:21 +0100

    Updated wishlist button

--- src/js/cssshapes.js
+++ src/js/cssshapes.js
@@ -1,3 +1,11 @@
 $( "#wishlist_button1" ).click(function() {
   $('.heart').toggleClass("added");
+  $('.tick').toggleClass("on");
+  $('.plus').toggleClass("on");
+});
+$("#wishlist_button1").mousedown(function() {
+	$('.heart').addClass("twist");
+});
+$("#wishlist_button1").mouseup(function() {
+	$('.heart').removeClass("twist");
 });
